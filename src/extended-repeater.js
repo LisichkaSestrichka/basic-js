c7126cc125a69fd474f7b5cc464758e4b79bbf31
LisichkaSestrichka <79520443+LisichkaSestrichka@users.noreply.github.com>
2021-03-15 13:50:10 +0300

    init: add other tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  if (repeatTimes === undefined) {
+  return '';
+}
+let masanys = [];
+const anys = [];
+
+for (let i = 0; i < repeatTimes; i++) {
+  masanys.push(str);
+}
+for (let i = 0; i < additionRepeatTimes; i++) {
+  anys.push(`${addition}`);
+}
+
+const anys2_0 = anys.join(additionSeparator);
+
+masanys = masanys.map((a) => a + anys2_0)
+
+return masanys.join(separator);
 };
-  
