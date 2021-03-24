577a6ce8eece36d0be629502edbea2a1b4811892
LisichkaSestrichka <79520443+LisichkaSestrichka@users.noreply.github.com>
2021-03-24 16:48:01 +0300

    iniy: added last task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,61 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification = true) {
+    this.isDirect = modification
   }
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    try {
+      let keyChars = key
+        .toUpperCase()
+        .split('')
+        .map((el) => el.charCodeAt(0))
+      let keyCounter = 0
+      let messageChars = message
+        .toUpperCase()
+        .split('')
+        .map((el) =>
+          /(?=[A-z])(?!\^)/u.test(el)
+            ? String.fromCharCode(
+                65 +
+                  ((el.charCodeAt(0) +
+                    keyChars[keyCounter++ % keyChars.length]) %
+                    26)
+              )
+            : el
+        )
+
+      return this.isDirect
+        ? messageChars.join('')
+        : messageChars.reverse().join('')
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
+  decrypt(message, key) {
+    let keyChars = key
+      .toUpperCase()
+      .split('')
+      .map((el) => el.charCodeAt(0))
+    let keyCounter = 0
+    let messageChars = message
+      .toUpperCase()
+      .split('')
+      .map((el) =>
+        /(?=[A-z])(?!\^)/u.test(el)
+          ? String.fromCharCode(
+              65 +
+                ((el.charCodeAt(0) -
+                  keyChars[keyCounter++ % keyChars.length] +
+                  26) %
+                  26)
+            )
+          : el
+      )
+    return this.isDirect
+      ? messageChars.join('')
+      : messageChars.reverse().join('')
   }
 }
 
-module.exports = VigenereCipheringMachine;
+module.exports = VigenereCipheringMachine
